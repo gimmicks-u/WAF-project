5b56ee898c63918fbf1bc6e7950aebb564b8b260
gimmicks-u <is@pusan.ac.kr>
2025-09-08 14:33:38 +0900

    init: install nestjs

--- backend-server/src/main.ts
+++ backend-server/src/main.ts
@@ -1,19 +1,8 @@
-import express from 'express';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
-
-app.get('/health', (req, res) => {
-  res.send('OK');
-});
-
-// 서버 시작
-app.listen(PORT, () => {
-  console.log(`Server on: http://localhost:${PORT}`);
-});
-
-export default app;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
